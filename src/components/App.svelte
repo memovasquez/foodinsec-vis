9823b069a6f838e21e9866d6ebffd34bd12fd6ef
Guillermo <guillermovasquez757@gmail.com>
2023-05-02 16:31:56 -0400

    GH Pages

---
+++ src/components/App.svelte
@@ -0,0 +1,30 @@
+<script>
+    //@ts-nocheck
+    import * as d3 from 'd3';
+    import {onMount} from 'svelte';
+
+    let fao_dataset_path = 'https://raw.githubusercontent.com/memovasquez/data/main/FAO_DatasetAbridged.csv';
+    let fao_data = [];
+    console.log("Outside onmount method");
+    onMount(async () => {
+        
+        d3.csv(fao_dataset_path).then((d) => {
+            fao_data = d;
+            console.log("Got data", d);
+        });
+
+        console.log("After then block");
+
+	});
+
+</script>
+
+<main>
+    <h1>App component</h1>
+    {#if fao_data.length >0 }
+    <p>{fao_data[0]}</p>
+    {/if}
+</main>
+
+
+
